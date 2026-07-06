e7180a27eda59f4a92670aeddfaf23d15b6e27ba
LiamDeSaldanha <129397980+LiamDeSaldanha@users.noreply.github.com>
2026-07-06 21:23:55 +0200

    Add: validate-parentheses - submission-11

---
+++ Data Structures & Algorithms/validate-parentheses/submission-11.java
@@ -0,0 +1,38 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character,Character> map = new HashMap<>();
+
+        map.put('(', ')');
+        map.put('{', '}');
+        map.put('[', ']');
+        
+
+        for(char c:s.toCharArray()){
+            if(map.containsKey(c)){
+                stack.push(map.get(c));
+
+            }else{
+                if(stack.isEmpty()){
+                    return false;
+                }
+                if(stack.pop()!=c){
+                    return false;
+                }
+
+                
+
+            }
+
+        
+        }
+        if(!stack.isEmpty()){
+            return false;
+        }
+        return true;
+
+
+
+
+    }
+}
